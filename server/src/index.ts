a7d953a720f6884daa85f07eac61c1b741e7edcc
Willian Duarte <willianm.cduarte@gmail.com>
2021-06-08 00:33:07 -0300

    connecting to mongoose database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import mongoose from "mongoose"
 import { json } from "body-parser"
 import { highScoresRouter } from "./routes/highScores"
 
@@ -6,6 +7,13 @@ const app = express()
 app.use(json())
 app.use(highScoresRouter)
 
+mongoose.connect("mongodb://localhost:27017/highScores", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, () => {
+    console.log("Connected to the database")
+})
 
 app.listen(9000, () => {
     console.log("Server is listening on port 9000")
